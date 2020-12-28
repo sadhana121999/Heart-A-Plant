f4318ea446b7a1659bbc58f599ec257e6087a9de
Sadhana Lokesh <31585033+sadhana121999@users.noreply.github.com>
2020-12-28 11:06:32 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,11 @@
+<?php
+// include header.php file
+include ('header.php');
+?>
+
+<!-- <section id="thankyou" > -->
+        <div class="container" style="width: 62%; margin:auto;">
+        <p>
+            <img src="./assets/thanks.png" alt="Thank You for shopping with us !!" width="900" ></p>
+        </div>
+<!-- </section> -->
